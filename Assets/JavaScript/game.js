b5b213d89493c1696ab6c5f353a43578c78c5fb9
kmstern4 <kmstern4@gmail.com>
2018-11-07 19:45:27 -0700

    defined basic variables and added 2 functions to change score and remaining guesses on html

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,2 +1,39 @@
+// VARIABLES
+var words = ["anemone", "barnacle", "barracuda", "dolphin", "flounder", "hammerhead", "jellyfish", "lobster", "manatee", "narwhal", "octopus", "oyster", "seahorse", "seaweed", "shipwreck", "shrimp", "stingray", "submarine", "swordfish", "tsunami"];
 
-var oceanWords = ["anemone", "barnacle", "barracuda", "dolphin", "flounder", "hammerhead", "jellyfish", "lobster", "manatee", "narwhal", "octopus", "oyster", "seahorse", "seaweed", "shipwreck", "shrimp", "stingray", "submarine", "swordfish", "tsunami"]
+var score = 0
+var currentWord = " ";
+var guesses = 12;
+var already = " ";
+var wordsIndex = 0;
+
+
+
+// FUNCTIONS
+
+//updates score
+function updateScore() {
+    document.getElementById("scoreText").innerHTML = score;
+}
+//updates remaining guesses
+function updateGuesses() {
+    document.getElementById("guessesText").innerHTML = guesses;
+}
+
+
+
+// MAIN PROCESS
+
+//choose random word from array
+
+//user guesses letter
+//if correct, letter replaces spot in word
+//if wrong, letter shows up in guessed, and remaining guesses decreases
+
+//letters already guessed should not be repeated
+
+//when full word is guessed, score increases, sound plays, new word is chosen
+
+document.onkeyup = function(event) {
+    var userInput = event.key.toLowerCase;
+}
