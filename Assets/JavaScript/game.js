f8e1fca91aec40043d9316d28e07476de8b6bed9
kmstern4 <kmstern4@gmail.com>
2018-11-07 18:35:56 -0700

    created basic html format, added array of words to be guessed

---
+++ Assets/JavaScript/game.js
@@ -0,0 +1,2 @@
+
+var oceanWords = ["anemone", "barnacle", "barracuda", "dolphin", "flounder", "hammerhead", "jellyfish", "lobster", "manatee", "narwhal", "octopus", "oyster", "seahorse", "seaweed", "shipwreck", "shrimp", "stingray", "submarine", "swordfish", "tsunami"]
